8eeffb6ae7fe2960e0a2c1612dd6aa0c3d9c813b
Ossai Lucky <luckyblaqy@gmail.com>
2022-09-18 21:00:34 +0100

    add newsletter subscribe section and the footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <div className="fLists">
+            <ul className="fList">
+                <li className="fListItem">Countries</li>
+                <li className="fListItem">Regions</li>
+                <li className="fListItem">Cities</li>
+                <li className="fListItem">Districts</li>
+                <li className="fListItem">Airports</li>
+                <li className="fListItem">Hotels</li>
+                
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Homes</li>
+                <li className="fListItem">Apartments</li>
+                <li className="fListItem">Resorts</li>
+                <li className="fListItem">Villas</li>
+                <li className="fListItem">Hostels</li>
+                <li className="fListItem">B&Bs</li>
+                <li className="fListItem">Guest Houses</li>
+                
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Unique places to stay</li>
+                <li className="fListItem">All destination</li>
+                <li className="fListItem">Discover</li>
+                <li className="fListItem">Districts</li>
+                <li className="fListItem">Reviews</li>
+                <li className="fListItem">Discover monthly stays</li>
+                <li className="fListItem">Unpacked: Travel articles</li>
+                <li className="fListItem">Travel communities</li>
+                <li className="fListItem">Seasonal and holiday deals</li>
+
+                
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Car rental</li>
+                <li className="fListItem">Flight finder</li>
+                <li className="fListItem">Resturants reservations</li>
+                <li className="fListItem">Bookme.com for Travel Agents</li>
+                
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Coronavirus (COVID-19) - FAQs </li>
+                <li className="fListItem">About Bookme.com</li>
+                <li className="fListItem">Customer Service Help</li>
+                <li className="fListItem">Partner help</li>
+                <li className="fListItem">Careers</li>
+                <li className="fListItem">Press center</li>
+                <li className="fListItem">Safety Resource Center</li>
+                <li className="fListItem">Investor relations</li>
+                <li className="fListItem">Terms & conditions</li>
+                <li className="fListItem">Partner dispute</li>
+                <li className="fListItem">How We Work</li>
+                <li className="fListItem">Privacy & cookie statement</li>
+                <li className="fListItem">MSA statement</li>
+                <li className="fListItem">Corperate contact</li>
+
+
+
+
+                
+            </ul>
+        </div>
+        <div className="fText">Copyright © 1996–2022 Bookme.com™. All rights reserved.
+</div>
+    </div>
+  )
+}
+
+export default Footer
